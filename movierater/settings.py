6d0f3afeb0209d76d3990d33e7f1b12d2507a878
Kuba <[inkub9999@gmail.com]>
2024-03-31 18:17:35 +0200

    ch

--- movierater/settings.py
+++ movierater/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p!n(b!wr_0be0ph@!0da!2oee-!q)pzqx*_rk25g(hzx^x*@bx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['13.51.200.0']
 
 
 # Application definition
